da8e59ef138b3b761f49ecacebf18882d14276fe
Romane Chouteau <romane.chouteau@makemepulse.com>
2025-04-07 18:19:15 +0200

    :sparkles: add keys

--- components/Tray.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="bg-gray-100 rounded-32 p-16 shadow-[12px_12px_0px_8px_var(--colors-gray-500)]">
-    <div class="w-full h-full bg-gray-200 rounded-24 shadow-[inset_32px_32px_0px_0_var(--colors-gray-300)]">
-    </div>
-  </div>
-</template>
--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -3,7 +3,11 @@
     <div class="w-full h-full grid grid-cols-12 gap-grid-gap">
       <div class="col-span-3 flex flex-col gap-grid-gap">
         <StampSheet/>
-        <div class="w-full h-full bg-gray-50"></div>
+        <div class="relative w-full h-full">
+          <img
+            src="@/assets/images/keys.svg"
+            class="absolute top-0 left-0 w-[101%] max-w-none h-auto"/>
+        </div>
       </div>
       <Envelope class="col-span-6" />
       <div class="col-span-3 bg-gray-50"></div>

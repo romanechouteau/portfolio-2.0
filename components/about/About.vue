83ff5e3c261681ae86b5e1db9daa50871d1775d2
Romane Chouteau <romane.chouteau@makemepulse.com>
2025-04-18 17:44:27 +0200

    :sparkles: add DS

--- components/about/About.vue
+++ components/about/About.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="col-span-6">
     <div class="flex flex-col gap-grid-gap">
-      <DS class="w-full" />
+      <div class="relative w-full aspect-[42/23]">
+        <img
+          src="@/assets/images/ds.svg"
+          class="absolute top-0 left-0 w-[100.7%] max-w-none h-auto"/>
+      </div>
       <div class="grid grid-cols-6 gap-grid-gap">
         <Mp3 class="col-span-4" />
         <div class="col-span-2 bg-gray-50"></div>
